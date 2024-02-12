799d9c1b6af1fc330b0c122e78de1c6e3f56ae0f
Zeng Ke <superisaac.ke@gmail.com>
2024-02-12 12:15:57 +0800

    matcher renamed to pattern

--- src/token.rs
+++ src/token.rs
@@ -10,57 +10,52 @@ struct Token {
 }
 
 impl fmt::Display for Token {
-    // This trait requires `fmt` with this exact signature.
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        // Write strictly the first element into the supplied output
-        // stream: `f`. Returns `fmt::Result` which indicates whether the
-        // operation succeeded or failed. Note that `write!` uses syntax which
-        // is very similar to `println!`.
         write!(
             f,
-            "{}:{} at [{}..{}]",
-            self.token,
-            self.value,
+            "[{}..{}] {} '{}'",
             self.position,
-            self.value.len() + self.position
+            self.value.len() + self.position,
+            self.token,
+            self.value
         )
     }
 }
 
 #[derive(Clone)]
-struct TokenMatcher {
+struct TokenPattern {
     token: &'static str,
     reg: Option<Regex>,
 }
 
 lazy_static! {
-    static ref TOKEN_MATCHERS: Vec<TokenMatcher> = {
-        let mut matchers: Vec<TokenMatcher> = Vec::new();
-        matchers.push(TokenMatcher {
+    static ref TOKEN_PATTERNS: Vec<TokenPattern> = {
+        let mut patterns: Vec<TokenPattern> = Vec::new();
+        patterns.push(TokenPattern {
             token: "space",
             reg: Some(Regex::new(r"^\s+").unwrap()),
         });
-        matchers.push(TokenMatcher {
+        patterns.push(TokenPattern {
             token: "comment_singleline",
             reg: Some(Regex::new(r"^//.*\n").unwrap()),
         });
-        matchers.push(TokenMatcher {
+        patterns.push(TokenPattern {
             token: "comment_multiline",
             reg: Some(Regex::new(r"^/\*(.|\n)*\*/").unwrap()),
         });
-        matchers.push(TokenMatcher{
-            token: "keywords",
+        patterns.push(TokenPattern{
+            token: "keyword",
             reg: Some(Regex::new(r"^\b(true|false|and|or|null|function|if|then|else|loop|for|some|every|in|return|satisfies)\b").unwrap()),
         });
-        matchers.push(TokenMatcher {
+        patterns.push(TokenPattern {
             token: "temporal",
             reg: Some(Regex::new(r#"^@"(\\.|[^"])*""#).unwrap()),
         });
-        matchers.push(TokenMatcher {
+        patterns.push(TokenPattern {
             token: "string",
             reg: Some(Regex::new(r#"^"(\\.|[^"])*""#).unwrap()),
         });
-        matchers.push(TokenMatcher {
+        patterns.push(TokenPattern {
             token: "number",
             reg: Some(Regex::new(r#"^\-?[0-9]+(\.[0-9]+)?\b"#).unwrap()),
         });
@@ -70,19 +65,22 @@ lazy_static! {
             "{", "}", ":=", ":", "+", "-", "*", "/", "%",
         ];
         for op in ops {
-            matchers.push(TokenMatcher {
+            patterns.push(TokenPattern {
                 token: op,
                 reg: None,
             });
         }
 
-        matchers.push(TokenMatcher{
+        patterns.push(TokenPattern{
             token: "name",
             //reg: Some(Regex::new(r"^[a-zA-Z_][a-zA-Z_0-9]*( +[a-zA-Z_][a-zA-Z_0-9]*)*").unwrap()),
-            reg: Some(Regex::new(r"[a-zA-Z_\$\p{Han}\p{Greek}\p{Bopomofo}\p{Hangul}][a-zA-Z_\$0-9\p{Han}\p{Greek}}\p{Bopomofo}\p{Hangul}]*").unwrap()),
+            reg: Some(Regex::new(r"(?x)
+            ^[a-zA-Z_\$\p{Han}\p{Greek}\p{Bopomofo}\p{Hangul}][a-zA-Z_\$0-9\p{Han}\p{Greek}}\p{Bopomofo}\p{Hangul}]*
+            (\s+[a-zA-Z_\$\p{Han}\p{Greek}\p{Bopomofo}\p{Hangul}][a-zA-Z_\$0-9\p{Han}\p{Greek}}\p{Bopomofo}\p{Hangul}]*)*
+            ").unwrap()),
         });
 
-        matchers
+        patterns
     };
 }
 
@@ -113,22 +111,22 @@ impl TokenScanner<'_> {
             });
         }
         let rest = &self.input[self.cursor..];
-        for matcher in TOKEN_MATCHERS.iter() {
-            if let Some(reg) = &matcher.reg {
+        for pattern in TOKEN_PATTERNS.iter() {
+            if let Some(reg) = &pattern.reg {
                 if let Some(m) = reg.find(rest) {
                     assert_eq!(0, m.start());
                     let token = Token {
-                        token: String::from(matcher.token),
+                        token: String::from(pattern.token),
                         value: m.as_str().to_string(),
                         position: self.cursor,
                     };
                     self.cursor += token.value.len();
                     return Ok(token);
                 }
-            } else if rest.starts_with(matcher.token) {
+            } else if rest.starts_with(pattern.token) {
                 let token = Token {
-                    token: String::from(matcher.token),
-                    value: String::from(matcher.token),
+                    token: String::from(pattern.token),
+                    value: String::from(pattern.token),
                     position: self.cursor,
                 };
                 self.cursor += token.value.len();
@@ -153,7 +151,7 @@ impl TokenScanner<'_> {
 }
 
 pub fn parse_token() {
-    let input = "1 world我";
+    let input = "1 world我 but(a+b) true";
     let mut scanner = TokenScanner::new(input);
     match scanner.find_tokens() {
         Ok(tokens) => {
